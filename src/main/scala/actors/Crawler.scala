6cdd05f4e19153b367889b13bedc9a5145ae37cf
matimiodosky <matias.mioodosky@ing.austral.edu.ar>
2020-01-11 20:52:44 -0300

    routered crawler actor

--- src/main/scala/actors/Crawler.scala
+++ src/main/scala/actors/Crawler.scala
@@ -3,9 +3,11 @@ package actors
 import actors.history.HistoryProvider
 import akka.actor.{Actor, ActorRef, Props, _}
 import akka.pattern.ask
+import akka.routing.{ActorRefRoutee, RoundRobinRoutingLogic, Router}
 import akka.util.Timeout
 import messages._
 import util.{Configuration, URLConsumer, URLConsumerProvider, URLUtil}
+
 import scala.collection.immutable.List
 import scala.concurrent.Await
 import scala.concurrent.duration._
@@ -13,27 +15,46 @@ import scala.language.postfixOps
 
 case class Crawler(history: ActorRef) extends Actor {
 
+  var router: Router = {
+    val workers = Vector.fill(Configuration.getConfig("crawlerWorkers") toInt) {
+      val r = context.actorOf(Props(CrawlerWorker(history)))
+      context.watch(r)
+      ActorRefRoutee(r)
+    }
+
+    Router(RoundRobinRoutingLogic(), workers)
+  }
+
+
+  override def receive: Receive = {
+    case work => router.route(work, sender)
+  }
+}
+
+case class CrawlerWorker(history: ActorRef) extends Actor {
 
   val parser: ActorRef = context.actorOf(Props[Parser])
   var client: ActorRef = ActorRef.noSender
   val fetcher: ActorRef = context.actorOf(Props(Fetcher(Configuration.getConfig("fetcherWorkers") toInt)))
   var toValidate: List[String] = List()
   val newURLConsumer: URLConsumer = URLConsumerProvider.getConsumer(Configuration.getConfig("URLConsumer"))
-
+  val start = System.currentTimeMillis()
   implicit val timeout: Timeout = Timeout(5 seconds)
+
 
   override def receive: Receive = {
 
     case Start(url) =>
       client = sender
       Await.result(history ? Clean(), 1 days)
-      history ! ValidateAsNewURL(url)
+      fetcher ! Fetch(url)
 
     case Fetched(url, html) => parser ! Parse(url, html)
 
     case Parsed(urls) =>
-      val max: Int = Configuration.getConfig("maxToValidate").toInt
-      toValidate = toValidate ::: urls.filter(url => URLUtil.getHost(url).equals(Configuration.getConfig("pageFilter")))
+      var max = Configuration.getConfig("maxToValidate").toInt
+      toValidate = toValidate ::: urls
+        .filter(url => URLUtil.getHost(url).equals(Configuration.getConfig("pageFilter")))
       toValidate.take(max).foreach(history ! ValidateAsNewURL(_))
       toValidate = toValidate.drop(max)
 
@@ -48,5 +69,4 @@ case class Crawler(history: ActorRef) extends Actor {
       println("Time: " + time + " Count: " + count + " -- Per Second: " + perSecond + "  -- Per URL (millis): " + perURL + " -- To Validate: " + toValidate.size)
 
   }
-
 }
